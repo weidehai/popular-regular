819aa4f41e47ca57cb2748394a5fdff8f9520b97
weidehai <243395655@qq.com>
2021-06-18 19:22:48 +0800

    add test

--- src/popularRegular.js
+++ src/popularRegular.js
@@ -1,10 +1,17 @@
-const pureNumber = "\\d+"
-const pureFloat = "((\\d*\\.\\d+$)|(\\d+\\.\\d*))"
-const signFloat = "(([+-]+\\d*\\.\\d+)|([+-]+\\d+\\.\\d*))"
-const signNumber = "[+-]+\\d+"
-const leftParentheses = "\\("
-const rightParentheses = "\\)"
-const whiteSpace = "\\s"
+const pureNumber = "\\d+";
+const pureFloat = "((\\d*\\.\\d+$)|(\\d+\\.\\d*))";
+const signFloat = "(([+-]+\\d*\\.\\d+)|([+-]+\\d+\\.\\d*))";
+const signNumber = "[+-]+\\d+";
+const leftParentheses = "\\(";
+const rightParentheses = "\\)";
+const whiteSpace = "\\s";
 
-
-
+export {
+  pureNumber,
+  pureFloat,
+  signFloat,
+  signNumber,
+  leftParentheses,
+  rightParentheses,
+  whiteSpace,
+};
---
+++ test/main.test.js
@@ -0,0 +1,49 @@
+import {
+  pureNumber,
+  pureFloat,
+  signFloat,
+  signNumber,
+  leftParentheses,
+  rightParentheses,
+  whiteSpace,
+} from '../src/popularRegular'
+
+
+test('pureNumber', () => {
+  expect(new RegExp(pureNumber).test('123')).toBe(true);
+});
+
+test('pureFloat', () => {
+  expect(new RegExp(pureFloat).test('1.123')).toBe(true);
+});
+
+test('pureFloat limit strat and end', () => {
+  expect(new RegExp(`^(${pureFloat})$`).test('1.123')).toBe(true);
+});
+
+
+test('pureFloat|signFloat (limit strat and end)', () => {
+  expect(new RegExp(`^((${pureFloat})|(${signNumber}))$`).test('-123')).toBe(true);
+});
+
+test('signFloat', () => {
+  expect(new RegExp(signFloat).test('+.123')).toBe(true);
+});
+
+test('signNumber', () => {
+  expect(new RegExp(signNumber).test('--123--')).toBe(true);
+});
+
+test('leftParentheses', () => {
+  expect(new RegExp(leftParentheses).test('(')).toBe(true);
+});
+
+test('rightParentheses', () => {
+  expect(new RegExp(rightParentheses).test(')')).toBe(true);
+});
+
+test('whiteSpace', () => {
+  expect(new RegExp(whiteSpace).test('   123')).toBe(true);
+});
+
+
